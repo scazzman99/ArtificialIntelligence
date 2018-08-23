79f17f64226356cb5f5697d06684927b5af0e630
scazzman99 <scazzman99@gmail.com>
2018-08-23 11:45:21 +1000

    New package for big new lessons

---
+++ Assets/Intro/Scripts/PlayerMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace Intro {
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerMove : MonoBehaviour {
+
+        public float speed = 8f;
+        public float jumpSpeed = 8f;
+        public float gravity = 20f;
+        Vector3 moveDirection = Vector3.zero;
+        public CharacterController controller;
+
+        // Use this for initialization
+        void Start() {
+            controller = GetComponent<CharacterController>();
+        }
+
+        // Update is called once per frame
+        void Update() {
+            if (controller.isGrounded)
+            {
+                moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+                moveDirection = transform.TransformDirection(moveDirection);
+                moveDirection *= speed;
+
+                if (Input.GetButton("Jump"))
+                {
+                    moveDirection.y = jumpSpeed;
+                }
+
+            }
+            moveDirection.y -= gravity * Time.deltaTime;
+            controller.Move(moveDirection * Time.deltaTime);
+        }
+
+
+    }
+}
---
+++ Assets/SteeringBehaviours/Scripts/AI/AIAgent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+    namespace SteeringBehaviours {
+    public class AIAgent : MonoBehaviour {
+
+        #region Vars
+        public Transform target;
+        public NavMeshAgent agent;
+        #endregion
+
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+            agent.SetDestination(target.position);
+        }
+    }
+}
